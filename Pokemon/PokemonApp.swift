87898638c42ebaf0a84e8c7375b968a0a6b1b651
Daffashiddiq <daffashiddiq@gmail.com>
2023-09-20 16:06:05 +0700

    Initial Commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Fajriharish on 20/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
